41db10166a215e73d41d05bdf1fb1e84d52ff7ec
paradoxskin <1312269430@qq.com>
2023-08-25 00:53:07 +0800

    93/95

--- conversions/from_into.rs
+++ conversions/from_into.rs
@@ -35,10 +35,26 @@ impl Default for Person {
 // If while parsing the age, something goes wrong, then return the default of Person
 // Otherwise, then return an instantiated Person object with the results
 
-// I AM NOT DONE
-
 impl From<&str> for Person {
     fn from(s: &str) -> Person {
+        if s == "" {
+            Person::default()
+        }
+        else {
+            let sp: Vec<&str> = s.split(',').collect();
+            if sp.len() != 2 || sp[0] == "" || sp[1] == "" {
+                return Person::default();
+            }
+            if let Ok(age) = sp[1].parse() {
+                Person {
+                    name: sp[0].to_string(),
+                    age
+                }
+            }
+            else {
+                Person::default()
+            }
+        }
     }
 }
 
